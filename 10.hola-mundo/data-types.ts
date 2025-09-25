876584ef5a3ae79e0c9cef0bc54ca824787e1cbe
Alejandro Loayza <aloayzab88@gmail.com>
2025-09-24 23:59:22 -0500

    feat: add boolean examples to data types

---
+++ 10.hola-mundo/data-types.ts
@@ -0,0 +1,15 @@
+let firstName: string = 'Alejandro';
+let lastName: string = 'Loayza';
+let fullName: string = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+let number1: number = 10;
+let number2: number = 20;
+
+console.log(number1 + number2);
+
+let isActive: boolean = true;
+let isRunning: boolean = false;
+console.log('isActive', isActive);
+console.log('isRunning', !isRunning);
