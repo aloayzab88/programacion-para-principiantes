1ce26f147c8ec27d16957ac5555ab4da4d94abb1
Alejandro Loayza <aloayzab88@gmail.com>
2025-09-28 16:09:32 -0500

    feat: add examples for control structures, if, esle and else if

---
+++ primeros-pasos/10.hola-mundo/app.ts
@@ -0,0 +1,18 @@
+//ts-worksheet
+
+console.log('Start');
+
+let isTired :boolean = false;
+
+if (!isTired) {
+    console.log('Drink water');
+}
+
+if (isTired) {
+    console.log('Drink cofee');
+} else {
+    console.log('Drink water');
+    
+}
+
+console.log('End');
---
+++ tipos-de-datos-y-estructuras-de-control/28.if-else-anidado.ts
@@ -0,0 +1,11 @@
+//ts-worksheet
+
+let grade: number = 60;
+
+if (grade >= 60) {
+    console.log('Class passed');
+} else if (grade >= 50) {
+    console.log('Please study more');
+} else {
+    console.log('Class failed');   
+}
