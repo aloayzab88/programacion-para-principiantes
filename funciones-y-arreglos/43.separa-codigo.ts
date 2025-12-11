c78ad4a3396ec45d113836f1bb2952d160824a07
Alejandro Loayza <aloayzab88@gmail.com>
2025-12-11 14:24:15 -0500

    feat: add aritmethic helpers

---
+++ funciones-y-arreglos/43.separa-codigo.ts
@@ -0,0 +1,11 @@
+//ts-worksheet
+
+import {addTwoNumbers, substractTwoNumbers, timesTwoNumbers, divideTwoNumbers} from './helpers/math-helpers';
+
+let num1:number = 10;
+let num2:number = 20;
+
+console.log(`Add: ${addTwoNumbers(num1, num2)}`);
+console.log(`Substract: ${substractTwoNumbers(num1, num2)}`);
+console.log(`Times: ${timesTwoNumbers(num1, num2)}`);
+console.log(`Divide: ${divideTwoNumbers(num1, num2)}`);
