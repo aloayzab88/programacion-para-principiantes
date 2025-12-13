6289f520f65f03156006a08d298bd69375dee442
Alejandro Loayza <aloayzab88@gmail.com>
2025-12-12 22:30:36 -0500

    feat: add array example

---
+++ funciones-y-arreglos/46.ejercicio-basico-de-arreglos.ts
@@ -0,0 +1,9 @@
+//ts-worksheet
+let names:string[] = ['Alejandro', 'Sandra', 'Freya', 'Rocket'];
+let numbers:number[] = [1,2,3,4,5];
+
+console.log(names[1]);
+
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index]);
+}
