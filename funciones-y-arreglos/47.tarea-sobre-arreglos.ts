2e8bb878884f2f2ebe6d122bbf6dc6e3dd2c1d96
Alejandro Loayza <aloayzab88@gmail.com>
2025-12-13 11:30:15 -0500

    feat: add array task

---
+++ funciones-y-arreglos/47.tarea-sobre-arreglos.ts
@@ -0,0 +1,8 @@
+//ts-worksheet
+
+let employees:string[] = ['Alejandro', 'Sandra', 'Freya'];
+let salaries:number[] = [1500, 2400, 3200];
+
+for (let index = 0; index < employees.length; index++) {
+    console.log(`${employees[index]} has a salary of ${salaries[index]}`);
+}
