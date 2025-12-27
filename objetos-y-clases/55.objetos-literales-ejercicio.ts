1e52fb1d89ed8332695e8e48e04535e967d042d4
Alejandro Loayza <aloayzab88@gmail.com>
2025-12-26 22:00:05 -0500

    feat: add literal objects homework

---
+++ objetos-y-clases/55.objetos-literales-ejercicio.ts
@@ -0,0 +1,22 @@
+//ts-worksheet
+
+let car = {
+    year: 2025,
+    make: 'Toyoya',
+    model: 'Corolla FX',
+    tirm: 'Seda 4dr Gas Cyl: 4 Front WD'
+}
+
+let smartTv = {
+    maker: 'Samsumg',
+    year: 2025,
+    line: 'Vision AI',
+    size: '65"'
+}
+
+let youtubeVideo = {
+    aspectRatio: '16/9',
+    length: '10:35',
+    ads: true,
+    subtitles: ['en', 'es']
+}
