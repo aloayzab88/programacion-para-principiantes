69fb51c803f21d596b95970c3dd2aa54c3146036
Alejandro Loayza <aloayzab88@gmail.com>
2025-12-29 14:10:08 -0500

    feat: add methods with arguments

--- objetos-y-clases/58.clases-en-programacion.ts
+++ objetos-y-clases/58.clases-en-programacion.ts
@@ -4,6 +4,7 @@ class Car {
     maker: string;
     doors: number;
     fuelTank: number;
+    fuelTankCapacity: number;
     isRunning: boolean;
     type: string;
     manufacturingDate: string;
@@ -11,7 +12,8 @@ class Car {
     constructor() {
         this.maker = 'No brand';
         this.doors = 4;
-        this.fuelTank = 100;
+        this.fuelTank = 0;
+        this.fuelTankCapacity = 100;
         this.isRunning = false;
         this.type = 'No type';
         this.manufacturingDate = '00-00-0000'
@@ -25,9 +27,28 @@ class Car {
         this.isRunning = true;
         return `The car is on.`;
     }
+
+    fillTank(gas: number) {
+        if (gas <= 0 ) throw new Error('Added must be positive');
+
+        const newFuelTank: number = this.fuelTank + gas;
+
+        if (newFuelTank > this.fuelTankCapacity) {
+            const excedent = newFuelTank - this.fuelTankCapacity;
+            this.fuelTank = this.fuelTankCapacity;
+            return `Gas tank is full. ${excedent} was not added`;
+        } else {
+            this.fuelTank = newFuelTank;
+            return `Gas tanks is at ${this.fuelTank}`
+        }
+        
+    }
 }
 
 let myCar = new Car();
 console.log(myCar);
 myCar.turnOn();
 myCar.turnOn();
+myCar.fillTank(40);
+myCar.fillTank(50);
+myCar.fillTank(50);
