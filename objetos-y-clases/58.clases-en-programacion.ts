830a249cd3a308530cab4495169b6080a5ad2bbf
Alejandro Loayza <aloayzab88@gmail.com>
2025-12-29 23:01:45 -0500

    feat: add acces to properties and methods

--- objetos-y-clases/58.clases-en-programacion.ts
+++ objetos-y-clases/58.clases-en-programacion.ts
@@ -1,51 +1,67 @@
 //ts-worksheet
 
 class Car {
-    maker: string;
-    doors: number;
-    fuelTank: number;
-    fuelTankCapacity: number;
-    isRunning: boolean;
-    type: string;
-    manufacturingDate: string;
+    // This property can be read only at run time
+    readonly maker: string;
+    // this means that this property can't be set outside the class during development, doesn't work on the copiled file
+    readonly doors: number; 
+    // but this property with # can't be set or get even on the compiled file
+    #fuelTank: number; 
+    #fuelTankCapacity: number;
+    #isRunning: boolean;
+    readonly type: string;
+    // The property only can be used inside the class at run time, doesn't work at compilation.
+    private readonly manufacturingDate: string; 
 
-    constructor() {
-        this.maker = 'No brand';
-        this.doors = 4;
-        this.fuelTank = 0;
-        this.fuelTankCapacity = 100;
-        this.isRunning = false;
-        this.type = 'No type';
-        this.manufacturingDate = '00-00-0000'
+    constructor(maker: string, doors: number, fuelTank: number, fuelTankCapacity: number, type: string, manufacturingDate: string) {
+        this.maker = maker;
+        this.doors = doors;
+        this.#fuelTank = fuelTank;
+        this.#fuelTankCapacity = fuelTankCapacity;
+        this.#isRunning = false;
+        this.type = type;
+        this.manufacturingDate = manufacturingDate
+    }
+
+    get fuelTank(): number {
+        return this.#fuelTank;
+    }
+
+    get fuelTankCapacity(): number {
+        return this.#fuelTankCapacity;
+    }
+
+    get isRunning(): boolean {
+        return this.#isRunning;
     }
 
     turnOn() {
-        if (this.isRunning) return `The car is already on.`;
+        if (this.#isRunning) return `The car is already on.`;
 
-        if (this.fuelTank <= 0) return `The gas tank is empty.`
+        if (this.#fuelTank <= 0) return `The gas tank is empty.`
         
-        this.isRunning = true;
+        this.#isRunning = true;
         return `The car is on.`;
     }
 
     fillTank(gas: number) {
-        if (gas <= 0 ) throw new Error('Added must be positive');
+        if (gas <= 0 ) throw new Error('Added gas must be positive number');
 
-        const newFuelTank: number = this.fuelTank + gas;
+        const newFuelTank: number = this.#fuelTank + gas;
 
-        if (newFuelTank > this.fuelTankCapacity) {
-            const excedent = newFuelTank - this.fuelTankCapacity;
-            this.fuelTank = this.fuelTankCapacity;
+        if (newFuelTank > this.#fuelTankCapacity) {
+            const excedent = newFuelTank - this.#fuelTankCapacity;
+            this.#fuelTank = this.#fuelTankCapacity;
             return `Gas tank is full. ${excedent} was not added`;
         } else {
-            this.fuelTank = newFuelTank;
-            return `Gas tanks is at ${this.fuelTank}`
+            this.#fuelTank = newFuelTank;
+            return `Gas tanks is at ${this.#fuelTank}`
         }
         
     }
 }
 
-let myCar = new Car();
+let myCar = new Car('Toyota', 4, 0, 100, 'Sedan', '2025-12-29');
 console.log(myCar);
 myCar.turnOn();
 myCar.turnOn();
