2275e374f1f69cbb8aa88a899aa8df162a0d8cb8
Alejandro Loayza <aloayzab88@gmail.com>
2026-01-01 23:59:27 -0500

    feat: add triangle area

---
+++ ejercicios-de-programacion/03.triangle-area.ts
@@ -0,0 +1,7 @@
+//ts-worksheet
+
+// 3 - Calcule el área de un triangulo rectángulo que: Base 10 y altura 5
+
+const rightTriangleArea = (base: number, height: number): number => base*height/2;
+
+console.log(rightTriangleArea(10,5));
