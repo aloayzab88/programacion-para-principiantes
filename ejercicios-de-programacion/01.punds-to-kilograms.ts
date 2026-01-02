e26a52750bd1d79dc9f03f9c01898d6da4e376eb
Alejandro Loayza <aloayzab88@gmail.com>
2026-01-01 23:41:45 -0500

    feat: add lbsToKgs

---
+++ ejercicios-de-programacion/01.punds-to-kilograms.ts
@@ -0,0 +1,10 @@
+//ts-worksheet
+
+/* 
+1 - Convertir de Libras a Kilogramos. (1 libra = 0.453592 Kilogramos)
+Ejemplo: 150 libras = 68.0388 kilogramos 
+*/
+
+const lbsToKgs = (lbs: number) => `${lbs} lbs = ${lbs * 0.453592} kilograms`;
+
+console.log(lbsToKgs(2));
